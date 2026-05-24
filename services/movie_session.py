2742c0da5f66ec28b0384da81278e2c24ac39770
Ambra <trank13f@gmail.com>
2026-05-24 13:04:17 +0300

    update movie_session service and get_taken_seats func

--- services/movie_session.py
+++ services/movie_session.py
@@ -42,3 +42,11 @@ def update_movie_session(
 
 def delete_movie_session_by_id(session_id: int) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(movie_session_id: int) -> list[dict]:
+    return list(MovieSession.objects.filter(
+        id=movie_session_id
+    ).values(
+        "tickets__row",
+        "tickets__seat"))
