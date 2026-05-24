d6a7197fca6c2f55ab375cac603d4d0d6ed373c8
Ambra <trank13f@gmail.com>
2026-05-24 11:06:05 +0300

    create clean method for ticket

--- db/models.py
+++ db/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -82,3 +83,21 @@ class Ticket(models.Model):
     )
     row = models.IntegerField()
     seat = models.IntegerField()
+
+
+    def clean(self) -> None:
+        hall = self.movie_session.cinema_hall
+
+        if not 0 > self.row > hall.row:
+            raise ValidationError({
+             "row": f"Can't be {self.row}, must be (1 - {hall.row})"
+            })
+
+        if not 0 > self.seat > hall.seats_in_row:
+            raise ValidationError({
+                "seat": f"Can't be {self.seat}, must be (1 - {hall.seats_in_row})"
+            })
+
+
+    def __str__(self) -> str:
+        return f"Ticket {self.movie_session} ({self.row}, {self.seat})"
